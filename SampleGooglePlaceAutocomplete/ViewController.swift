67b803cd3a013a8846262f7cebec5508245033be
Alvin George <alvin@sodio.tech>
2017-07-10 20:17:22 +0530

    initial commit

--- SampleGooglePlaceAutocomplete/ViewController.swift
+++ SampleGooglePlaceAutocomplete/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import GooglePlaces
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GMSAutocompleteViewControllerDelegate {
+    
+    @IBOutlet weak var placeDetailTextView: UITextView!
+    
+    var  gmsController:GMSAutocompleteViewController =  GMSAutocompleteViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addInitialUISetup()
     }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        addInitialUISetup()
+    }
+    func addInitialUISetup(){
 
+        gmsController.delegate = self
+        customizeGooglePlaceSearch()
+    }
+    
+    @IBAction func launchGooglePicker(_ sender: Any) {
+        
+
+        present(gmsController, animated: true, completion: nil)
+        
+    }
+    func customizeGooglePlaceSearch()
+    {
+        gmsController.tintColor =  UIColor.darkGray
+        gmsController.tableCellBackgroundColor =  UIColor.white
+        gmsController.secondaryTextColor =  UIColor.darkGray
+        
+    }
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        
+        
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        
+        
+        placeDetailTextView.text = "Place name: \(place.name) \nPlace address: \(place.formattedAddress) \nPlace attributions: \(place.attributions)"
+        
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+        
+        
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: \(error)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // User cancelled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        print("Autocomplete was cancelled.")
+        dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
